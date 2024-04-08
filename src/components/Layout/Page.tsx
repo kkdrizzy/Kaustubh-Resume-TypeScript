2b97f254943eee8d4de9907aaa7443ac100a0379
kkdrizzy <kaustubhkatti.205@gmail.com>
2024-04-08 13:45:05 +0100

    Favicon page icon

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -14,13 +14,11 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <title>{title}</title>
         <meta content={description} name="description"/>
 
-        {/* several domains list the same content, make sure google knows we mean this one. */}
-        <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical"/>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
+        <link href="/favicon.ico" rel="icon" sizes="any"/>
+        <link href="/icon.svg" rel="icon" type="image/svg+xml"/>
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon"/>
+        <link href="/site.webmanifest" rel="manifest"/>
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title"/>
